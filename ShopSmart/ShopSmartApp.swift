d8e3b4183cb52f4ed80c03978856e86256b40f4e
alimkhann <alimkhan.ergebayev@gmail.com>
2025-04-25 21:18:10 +0800

    Initial Commit

---
+++ ShopSmart/ShopSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSmartApp.swift
+//  ShopSmart
+//
+//  Created by Alimkhan Yergebayev on 25/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopSmartTests/ShopSmartTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSmartTests.swift
+//  ShopSmartTests
+//
+//  Created by Alimkhan Yergebayev on 25/4/2025.
+//
+
+import Testing
+@testable import ShopSmart
+
+struct ShopSmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
